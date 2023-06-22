8c876fab5ed9ecf3e169537169f6d0658b6e27f1
Shrihari Murali <shriharim6@gmail.com>
2023-06-22 13:08:04 +0530

    initial commmit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 4000;
+const MONGO_URI = process.env.MONGO_URI;
+
+const authRoute = require("./routes/auth/auth");
+
+app.get("/", (req, res) => {
+  res.send(`Hey it's working !!`);
+});
+
+app.listen(PORT, () => console.log(`server up and running at  ${PORT}`));
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Successfully connected to database");
+  })
+  .catch((error) => {
+    console.log("database connection failed. exiting now...");
+    console.error(error);
+    process.exit(1);
+  });
+
+const whitelist = ["http://localhost:3000"];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+
+app.use(express.json(), cors(corsOptions));
+
+//ROUTE MIDDLEWARE
+app.use("/api/users", authRoute);
---
+++ routes/auth/auth.js
@@ -0,0 +1,112 @@
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const User = require("../../model/user");
+
+const Yup = require("yup");
+
+const registerSchema = Yup.object({
+  fname: Yup.string().min(3).required(),
+  lname: Yup.string().min(3).required(),
+  email: Yup.string().min(6).email().required(),
+  password: Yup.string().min(8).required(),
+});
+
+// Register Route
+
+router.post("/register", async (req, res) => {
+  const { fname, lname, email, password } = req.body;
+  const emailExist = await User.findOne({ email });
+
+  if (emailExist) {
+    return res.status(400).send("Email already exists.");
+  }
+
+  const salt = await bcrypt.genSaltSync(10);
+  const hashPassword = await bcrypt.hash(password, salt);
+
+  const user = new User({
+    fname,
+    lname,
+    email,
+    password: hashPassword,
+  });
+
+  try {
+    const { error } = await registerSchema.validateSync(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    await user.save();
+    return res.status(200).send("User create successfully!.");
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+
+const loginSchema = Yup.object({
+  email: Yup.string().min(6).required().email(),
+  password: Yup.string().min(6).required(),
+});
+
+// Login Route
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(400).send("User do not exists. Please Sign up");
+  }
+
+  const validPassword = await bcrypt.compare(password, user.password);
+
+  if (!validPassword) {
+    return res.status(400).send("Invalid Password");
+  }
+
+  try {
+    const { error } = await loginSchema.validateSync(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const accessToken = generateAccessToken({ id: user.id });
+    const refreshToken = generateRefreshToken({ id: user.id });
+
+    res.json({ accessToken, refreshToken, user: { ...getBasicDetails(user) } });
+  } catch (error) {
+    console.log({ error });
+    res.status(500).send(error);
+  }
+});
+
+function getBasicDetails(user) {
+  const { id, fname, lname, email } = user;
+  return { id, fname, lname, email };
+}
+
+router.post("/refresh", (req, res) => {
+  const refreshToken = req.body.refreshToken;
+  if (refreshToken == null) return res.sendStatus(401, "Unauthorized!");
+  jwt.verify(refreshToken, process.env.JWT_REFRESH_KEY, (err, user) => {
+    if (err) return res.sendStatus(403).send(err);
+    const accessToken = generateAccessToken({ id: user.id });
+    const refreshToken = generateRefreshToken({ id: user.id });
+    res.json({ accessToken, refreshToken });
+  });
+});
+
+function generateAccessToken(user) {
+  return jwt.sign({ id: user.id }, process.env.JWT_KEY, {
+    expiresIn: process.env.EXPIRES_IN,
+  });
+}
+
+function generateRefreshToken(user) {
+  return jwt.sign({ id: user.id }, process.env.JWT_REFRESH_KEY, {
+    expiresIn: process.env.REFRESH_EXPIRES_IN,
+  });
+}
+
+module.exports = router;
